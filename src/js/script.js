1d5707a3e3f068bcfba5de76a301b2a6de9bf5b5
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-02-14 15:21:51 -0300

    feat: Card cheats od player cheats, world cheats and vehicle cheats

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,54 @@
-import { playerCheats } from "../api_cheats/dados.js";
+import {
+  playerCheats,
+  worldCheats,
+  vehicleCheats,
+} from "../api_cheats/dados.js";
 
-let card = document.getElementById("card_player");
+function createCheatsCard(list_cheats, idCard) {
+  let card = document.getElementById(idCard);
 
-// Iterar sobre o array de objetos
-playerCheats.forEach((codigo) => {
-  // Criar elemento de card content
-  let card_content = document.createElement("div");
-  card_content.classList.add("card");
-  card_content.classList.add("mt-3");
-  card_content.classList.add("card__cheat")
-  card.appendChild(card_content);
+  // Iterar sobre o array de objetos
+  list_cheats.forEach((codigo) => {
+    // Criar elemento de card content
+    let card_content = document.createElement("div");
+    card_content.classList.add("card");
+    card_content.classList.add("mt-3");
+    card_content.classList.add("card__cheat");
+    card.appendChild(card_content);
 
-  // Criar elemento card body
-  let card_body = document.createElement("div");
-  card_body.classList.add("card-body");
-  card_content.appendChild(card_body);
+    // Criar elemento card body
+    let card_body = document.createElement("div");
+    card_body.classList.add("card-body");
+    card_content.appendChild(card_body);
 
-  // Criar elemento de card
-  let card_container = document.createElement("div");
-  card_container.classList.add("card__container");
-  card_body.appendChild(card_container);
+    // Criar elemento de card
+    let card_container = document.createElement("div");
+    card_container.classList.add("card__container");
+    card_body.appendChild(card_container);
 
-  // Criar elemento de titulo
-  const codigoTitulo = document.createElement("h5");
-  codigoTitulo.textContent = codigo.nome;
-  card_container.appendChild(codigoTitulo);
+    // Criar elemento de titulo
+    const codigoTitulo = document.createElement("h5");
+    codigoTitulo.textContent = `${codigo.id} - ${codigo.nome}`;
+    card_container.appendChild(codigoTitulo);
 
-  // Criar elemento de imagemn
-  let card_img = document.createElement("div");
-  card_img.classList.add("card__codigo");
-  card_container.appendChild(card_img);
+    // Criar elemento de imagemn
+    let card_img = document.createElement("div");
+    card_img.classList.add("card__codigo");
+    card_container.appendChild(card_img);
 
-  // Iterar sobre o array de imagens dentro de cada objeto
-  codigo.imagems.forEach((imagem) => {
-    // Criar elemento de imagem
-    const codigoImg = document.createElement("img");
-    codigoImg.classList.add("img");
-    codigoImg.src = imagem;
-    codigoImg.alt = "icone do cheat";
+    // Iterar sobre o array de imagens dentro de cada objeto
+    codigo.imagems.forEach((imagem) => {
+      // Criar elemento de imagem
+      const codigoImg = document.createElement("img");
+      codigoImg.classList.add("img");
+      codigoImg.src = imagem;
+      codigoImg.alt = "icone do cheat";
 
-    card_img.appendChild(codigoImg);
+      card_img.appendChild(codigoImg);
+    });
   });
-});
+}
+
+createCheatsCard(playerCheats, "card_player");
+createCheatsCard(worldCheats, "card_world");
+createCheatsCard(vehicleCheats, "card_vehicle");
