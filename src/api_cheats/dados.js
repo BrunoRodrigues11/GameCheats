bd1d4beb3f58de80adc8a9eb1e69169b8fcfebda
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-02-13 22:33:39 -0300

    feat: Cheats page

---
+++ src/api_cheats/dados.js
@@ -0,0 +1,48 @@
+const playerCheats = [
+  {
+    id: 1,
+    nome: "Todas as armas",
+    descricao:
+      "TRIÂNGULO, R2, ESQUERDA, L1, X, DIREITA, TRIÂNGULO, BAIXO, QUADRADO, L1, L1, L1",
+    plataforma: "PS",
+    secao: "Jogador",
+    imagems: [
+      "../src/assets/icons/ps/ps_Triangulo.svg",
+      "../src/assets/icons/ps/ps_R2.svg",
+      "../src/assets/icons/ps/ps_Esquerda.svg",
+      "../src/assets/icons/ps/ps_L1.svg",
+      "../src/assets/icons/ps/ps_X.svg",
+      "../src/assets/icons/ps/ps_Direita.svg",
+      "../src/assets/icons/ps/ps_Triangulo.svg",
+      "../src/assets/icons/ps/ps_Baixo.svg",
+      "../src/assets/icons/ps/ps_Quadrado.svg",
+      "../src/assets/icons/ps/ps_L1.svg",
+      "../src/assets/icons/ps/ps_L1.svg",
+      "../src/assets/icons/ps/ps_L1.svg",
+    ],
+  },
+  {
+    id: 2,
+    nome: "Vida máxima + armadura",
+    descricao:
+      "CÍRCULO, L1, TRIÂNGULO, R2, X, QUADRADO, CÍRCULO, DIREITA, QUADRADO, L1, L1, L1",
+    plataforma: "PS",
+    secao: "Jogador",
+    imagems: [
+      "../src/assets/icons/ps/ps_Bolinha.svg",
+      "../src/assets/icons/ps/ps_L1.svg",
+      "../src/assets/icons/ps/ps_Triangulo.svg",
+      "../src/assets/icons/ps/ps_R2.svg",
+      "../src/assets/icons/ps/ps_X.svg",
+      "../src/assets/icons/ps/ps_Quadrado.svg",
+      "../src/assets/icons/ps/ps_Bolinha.svg",
+      "../src/assets/icons/ps/ps_Direita.svg",
+      "../src/assets/icons/ps/ps_Quadrado.svg",
+      "../src/assets/icons/ps/ps_L1.svg",
+      "../src/assets/icons/ps/ps_L1.svg",
+      "../src/assets/icons/ps/ps_L1.svg",
+    ],
+  },
+];
+
+export { playerCheats}
---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+import { playerCheats } from "../api_cheats/dados.js";
+
+console.log(playerCheats);
+console.log(playerCheats[0].nome);
